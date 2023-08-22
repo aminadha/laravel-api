23db8b0f2c10040d501cfc5405005968c2fb137d
Amin Adha <aminadha@gmail.com>
2023-08-22 11:57:35 +0800

    add migration add columns organization

---
+++ database/migrations/2023_08_22_114414_add_status_to_organizations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dateTime('activated_at')->nullable()->after('staff_count');
+            $table->string('status')->default('inactive')->after('staff_count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn('activated_at');
+            $table->dropColumn('status');
+        });
+    }
+};
