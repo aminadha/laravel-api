a482cf460acad04ad21be124fcec308abf5e5f2b
Amin Adha <aminadha@gmail.com>
2023-08-23 11:10:19 +0800

    fix: auth controller namespace

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\V1\AuthController;
 use App\Http\Controllers\Api\V1\ActivateOrganization;
 use App\Http\Controllers\API\V1\BranchesController;
 use App\Http\Controllers\API\V1\EmployeesController;
